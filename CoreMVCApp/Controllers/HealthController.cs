61bb74e4517d56d6c5788a6b86ea02454b1edc42
isaeed <isaeed@macrosoftinc.com>
2023-04-14 15:24:19 +0500

    upgraded to .NET 6.0

---
+++ CoreMVCApp/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMVCApp.Controllers
+{
+    public class HealthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Health");
+        }
+    }
+}
